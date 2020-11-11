1078e326bcc7c8757beaa5bf9d5dcb661b0ad235
Clément <clement.gaultier@gmail.com>
2020-11-11 18:54:38 +0100

    Opt-out option on cookie banner + misc zoom in effects

---
+++ static/js/cookie-consent.js
@@ -0,0 +1,51 @@
+
+window.cookieconsent.initialise({
+  "palette": {
+    "popup": {
+      "background": "#252e39"
+    },
+    "button": {
+      "background": "#14a7d0"
+    }
+  },
+  "theme": "classic",
+  "type": "opt-out",
+  "content": {
+    "message": "This website uses cookies to ensure you get the best experience browsing my webpage.",
+    "dismiss": "Allow cookies",
+    "deny": "Decline",
+    "link": "Learn more"
+  },
+  onInitialise: function (status) {
+    var type = this.options.type;
+    var didConsent = this.hasConsented();
+    if (type == 'opt-in' && didConsent) {
+      // enable cookies
+      loadGAonConsent();
+    }
+    if (type == 'opt-out' && !didConsent) {
+      // disable cookies
+    }
+  },
+  onStatusChange: function(status, chosenBefore) {
+    var type = this.options.type;
+    var didConsent = this.hasConsented();
+    if (type == 'opt-in' && didConsent) {
+      // enable cookies
+      loadGAonConsent();
+    }
+    if (type == 'opt-out' && !didConsent) {
+      // disable cookies
+    }
+  },
+  onRevokeChoice: function() {
+    var type = this.options.type;
+    if (type == 'opt-in') {
+      // disable cookies
+    }
+    if (type == 'opt-out') {
+      // enable cookies
+      loadGAonConsent();
+    }
+  }
+});
